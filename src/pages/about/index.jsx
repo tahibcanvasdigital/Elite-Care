be615ad191df98c52077bc3141c6b400ef67d2b9
tahibcanvasdigital <muhammad.tahib@canvasdigital.net>
2024-01-23 16:18:50 +0500

    initial

---
+++ src/pages/about/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Styles from './style.module.css'
+
+const About = () => {
+  return (
+    <div className={Styles.container}>About</div>
+  )
+}
+
+export default About
